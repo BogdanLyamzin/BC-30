8ae68fcb1fcfcfb2b417f889737cf1401297b337
unknown <bogdan.lyamzin.d@gmail.com>
2023-01-23 18:52:13 +0200

    finish lesson 13

---
+++ lesson-13/google-auth-example/middlewares/index.js
@@ -0,0 +1,11 @@
+const validateBody = require("./validateBody")
+const authenticate = require("./authenticate")
+const isValidId = require("./isValidId")
+const passport = require("./passport")
+
+module.exports = {
+    validateBody,
+    authenticate,
+    isValidId,
+    passport,
+}
